b580b924b89e4517e44cb42e44d7670adf960025
vtrtga <vitor.ypvalim@gmail.com>
2022-11-15 16:43:55 -0400

    server fixed

--- app/backend/build/database/config/config.js
+++ app/backend/build/database/config/config.js
@@ -5,7 +5,7 @@ const config = {
     username: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '123456',
     database: 'PROCESSO_NG',
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'db',
     port: Number(process.env.DB_PORT) || 3306,
     dialect: 'postgres',
     dialectOptions: {
